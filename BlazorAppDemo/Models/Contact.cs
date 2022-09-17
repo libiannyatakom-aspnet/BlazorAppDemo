dddc048b19133c502506fecb7a8ffb1f5273ec62
Libian Nyata <libiannyatakom@gmail.com>
2022-09-17 17:07:59 +0700

    First commit

---
+++ BlazorAppDemo/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorAppDemo.Models
+{
+    public class Contact
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public DateTime created_at { get; set; }
+    }
+}
---
+++ BlazorAppDemo/Pages/ContactPage.razor.cs
@@ -0,0 +1,32 @@
+﻿using BlazorAppDemo.Models;
+
+namespace BlazorAppDemo.Pages
+{
+    public partial class ContactPage
+    {
+        private List<Contact> contacts;
+
+        protected async override Task OnInitializedAsync()
+        {
+            await Task.Delay(3000);
+
+            contacts = new List<Contact> {
+                new Contact {
+                    first_name = "Libian",
+                    last_name = "Nyata",
+                    email = "libiannyatakom@gmail.com",
+                    gender = "M",
+                    created_at = DateTime.Now
+                },
+                new Contact {
+                    first_name = "Adnan",
+                    last_name = "Aditya Pradipta",
+                    email = "adnan@gmail.com",
+                    gender = "M",
+                    created_at = DateTime.Now.AddMinutes(3)
+                }
+            };
+            base.OnInitializedAsync();
+        }
+    }
+}
