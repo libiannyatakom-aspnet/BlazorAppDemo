13e63e8dbbd3fdcb815e1c571d373cdbfa40db3a
Libian <libian@eskalink.com>
2022-09-19 12:23:40 +0700

    update demo

---
+++ BlazorAppDemo/Pages/ContactDemo.razor.cs
@@ -0,0 +1,33 @@
+﻿using BlazorAppDemo.Models;
+
+namespace BlazorAppDemo.Pages
+{
+    public partial class ContactDemo
+    {
+        private bool displayEmail = true;
+
+        private string first_name, last_name, email, gender;
+
+        private int age;
+
+        private void addContact()
+        {
+            var contact = new Contact
+            {
+                first_name = first_name,
+                last_name = last_name,
+                email = email,
+                gender = gender,
+                age = age,
+                created_at = DateTime.Now
+            };
+
+            contactService.addContact(contact);
+        }
+
+        private void deleteContact(Contact contact)
+        {
+            contactService.contactList.Remove(contact);
+        }
+    }
+}
---
+++ BlazorAppDemo/Services/ContactService.cs
@@ -0,0 +1,14 @@
+﻿using BlazorAppDemo.Models;
+
+namespace BlazorAppDemo.Services
+{
+    public class ContactService
+    {
+        public List<Contact> contactList = new List<Contact>();
+
+        public void addContact(Contact contact)
+        {
+            contactList.Add(contact);
+        }
+    }
+}
